5beb4d4ba6f7e39772e4544950a602d067ac6073
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2026-03-21 15:20:52 -0700

    Added code for opening an Open File Dialog for loading in raw data (still need to load in said data).

--- PowerBallStatsSLN/PowerBallStatsShell/MainWindowViewModel.cs
+++ PowerBallStatsSLN/PowerBallStatsShell/MainWindowViewModel.cs
@@ -22,7 +22,21 @@ public  class MainWindowViewModel
 	public ICommand LoadRawDataCommand { get; private set; }
 	public void LoadRawDataCommand_Execute() 
 	{
+		var dialog = new Microsoft.Win32.OpenFileDialog()
+		{
+			FileName	= "Document",
+			DefaultExt	= ".csv",
+			Filter		= "Comma-Separated Values (.csv)|*.csv",
+			Multiselect = false,
+			Title		= "Import Powerball Data"
+		};
 
+		bool keepGoing = dialog.ShowDialog() == true;
+
+		if (!keepGoing)
+			return;
+
+		var pathname = dialog.FileName;
 	}
 
 }
