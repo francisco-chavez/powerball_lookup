a9b781fd2e9cbdfa760667ddb32831d7dda01715
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2026-03-21 15:07:24 -0700

    Wiring some stuff up.

---
+++ PowerBallStatsSLN/PowerBallStatsShell/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace PowerBallStatsShell;
+
+public  class MainWindowViewModel
+	: ViewModelBase
+{
+	
+	public MainWindowViewModel()
+	{
+		LoadRawDataCommand = new DelegateCommand(LoadRawDataCommand_Execute);
+	}
+
+
+	public ICommand LoadRawDataCommand { get; private set; }
+	public void LoadRawDataCommand_Execute() 
+	{
+
+	}
+
+}
