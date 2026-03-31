06890c2e97bfccfbc8a3e8a0006f1cdbbf6d7f9f
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2026-03-31 12:26:46 -0700

    Revert "Removing WPF project."
    This reverts commit ebc9ee9c145556c1a614c80b9b08ff7543f5bf5b.

---
+++ PowerBallStatsSLN/PowerBallStatsShell/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace PowerBallStatsShell;
+
+public  class MainWindowViewModel
+	: ViewModelBase
+{
+	
+	public MainWindowViewModel()
+	{
+		LoadRawDataCommand = new DelegateCommand(LoadRawDataCommand_Execute);
+	}
+
+
+	public ICommand LoadRawDataCommand { get; private set; }
+	public void LoadRawDataCommand_Execute() 
+	{
+		var dialog = new Microsoft.Win32.OpenFileDialog()
+		{
+			FileName	= "Document",
+			DefaultExt	= ".csv",
+			Filter		= "Comma-Separated Values (.csv)|*.csv",
+			Multiselect = false,
+			Title		= "Import Powerball Data"
+		};
+
+		bool keepGoing = dialog.ShowDialog() == true;
+
+		if (!keepGoing)
+			return;
+
+		var pathname = dialog.FileName;
+	}
+
+}
