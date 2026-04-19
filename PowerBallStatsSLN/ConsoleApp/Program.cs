82d364d2138d1fd896a0f048fdbe0f0f99b31984
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2026-04-19 00:42:07 -0700

    Switched to using the winning number with the greatest weight, and then selecting numbers that have come up with that number.

--- PowerBallStatsSLN/ConsoleApp/Program.cs
+++ PowerBallStatsSLN/ConsoleApp/Program.cs
@@ -185,7 +185,9 @@ internal class Program
 		int winningsWithPowerPlay = 0;
 		int spendingsWithPowerPlay = 0;
 		int timesPlayed		= 0;
-		var decayValue		= 0.99;
+		var decayValue		= 0.925;
+
+		var set_weight_matrix = new double[70,70];
 
 		for (var i = 0; i < powerballDrawings.Count; i++)
 		{
@@ -197,7 +199,9 @@ internal class Program
 			{
 				timesPlayed++;
 
-				var predictedNumbers = PredictNumbers(winningNumberCounts, powerballCounts);
+				//var predictedNumbers = PredictNumbers(winningNumberCounts, powerballCounts);
+				var predictedNumbers = PredictNumbers(set_weight_matrix, powerballCounts);
+
 				var drawWinnings = powerballDrawings[i].CalculateWinnings(predictedNumbers.Item1, predictedNumbers.Item2, false);
 				var drawWinningsWithPowerplay = powerballDrawings[i].CalculateWinnings(predictedNumbers.Item1, predictedNumbers.Item2, true);
 				winnings += drawWinnings;
@@ -207,11 +211,11 @@ internal class Program
 				
 
 				Console.WriteLine($"Date: {powerballDrawings[i].Date}");
-				Console.WriteLine($"\tWinning Numbers: {{{string.Join(", ", winningNumbers)}}}");
+				Console.WriteLine($"\tWinning Numbers: {{ {string.Join(", ", winningNumbers)} }}");
 				Console.WriteLine($"\tPowerball Number: {powerballNumber}");
 				Console.WriteLine($"\tPowerplay Multiplier: {multiplier}");
 				Console.WriteLine();
-				Console.WriteLine($"\tPredicted Winning Numbers: {{{string.Join(", ", predictedNumbers.Item1)}}}");
+				Console.WriteLine($"\tPredicted Winning Numbers: {{ {string.Join(", ", predictedNumbers.Item1)} }}");
 				Console.WriteLine($"\tPredicted Powerball Number: {predictedNumbers.Item2}");
 				Console.WriteLine();
 				Console.WriteLine("\tNegative values for standard USD are embedded inside of '()'.");
@@ -223,6 +227,22 @@ internal class Program
 				Console.WriteLine($"\t\tWith Powerplay: ${winningsWithPowerPlay - spendingsWithPowerPlay:C}");
 				Console.WriteLine();
 			}
+
+			for (var j = 1; j < 70; j++)
+				for (var k = 1; k < 70; k++)
+					set_weight_matrix[j, k] *= decayValue;
+
+			foreach (var n in winningNumbers)
+			{
+				foreach (var m in winningNumbers)
+				{
+					if (n == m)
+						continue;
+					set_weight_matrix[n, m] += 1.0;
+				}
+				set_weight_matrix[n, n] += 1.0;
+			}
+
 
 			for (var j = 1; j < winningNumberCounts.Length; j++)
 				winningNumberCounts[j] *= decayValue;
@@ -241,9 +261,9 @@ internal class Program
 		var weightedWinningNumbers = new List<Tuple<int, double>>(70);
 		var weightedPowerballNumbers = new List<Tuple<int, double>>(27);
 
-		for (int i = 0; i < winningNumberWeights.Length; i++)
+		for (int i = 1; i < winningNumberWeights.Length; i++)
 			weightedWinningNumbers.Add(new Tuple<int, double>(i, winningNumberWeights[i]));
-		for (int i = 0; i < powerballWeights.Length; i++)
+		for (int i = 1; i < powerballWeights.Length; i++)
 			weightedPowerballNumbers.Add(new Tuple<int, double>(i, powerballWeights[i]));
 
 		weightedWinningNumbers.Sort(SortByGreatestWeightFirst);
@@ -253,6 +273,58 @@ internal class Program
 
 		for (int i = 0; i < 5; i++)
 			predictedNumbers[i] = weightedWinningNumbers[i].Item1;
+
+		return new Tuple<int[], int>(predictedNumbers, weightedPowerballNumbers[0].Item1);
+	}
+
+	private static Tuple<int[], int> PredictNumbers(double[,] winning_numbers_set_weight_matrix, double[] powerball_numbers_weights)
+	{
+		var weightedPowerballNumbers = new List<Tuple<int, double>>(27);
+		for (int i = 0; i < powerball_numbers_weights.Length; i++)
+			weightedPowerballNumbers.Add(new Tuple<int, double>(i, powerball_numbers_weights[i]));
+		weightedPowerballNumbers.Sort(SortByGreatestWeightFirst);
+
+
+		var predictedNumbers = new int[5];
+		var max_weight = -1.0;
+		var value_with_max_weight = 0;
+
+		var additionalWeights = 0.0;
+		for (int i = 1; i < 70; i++)
+		{
+			if (winning_numbers_set_weight_matrix[i, i] >= max_weight)
+			{
+				var t = 0.0;
+				for (int j = 1; j < 70; j++)
+					t += winning_numbers_set_weight_matrix[i, j];
+
+				if (winning_numbers_set_weight_matrix[i, i] > max_weight)
+				{
+					value_with_max_weight = i;
+					max_weight = winning_numbers_set_weight_matrix[i, i];
+
+					additionalWeights = t;
+				}
+				else
+				{
+					if (t > additionalWeights)
+					{
+						value_with_max_weight = i;
+						max_weight = winning_numbers_set_weight_matrix[i, i];
+						additionalWeights = t;
+					}
+				}
+			}
+		}
+
+		var weightedWinningNumbers = new List<Tuple<int, double>>(69);
+		for (int i = 1; i < 70; i++)
+			weightedWinningNumbers.Add(new Tuple<int, double>(i, winning_numbers_set_weight_matrix[value_with_max_weight, i]));
+		weightedWinningNumbers.Sort(SortByGreatestWeightFirst);
+
+		for (int i = 0; i < 5; i++)
+			predictedNumbers[i] = weightedWinningNumbers[i].Item1;
+
 
 		return new Tuple<int[], int>(predictedNumbers, weightedPowerballNumbers[0].Item1);
 	}
